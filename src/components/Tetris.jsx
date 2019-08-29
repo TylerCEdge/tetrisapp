5c374e1d7cb61e918523e020978e2040b45383bc
TylerCEdge <tylercedge@gmail.com>
2019-08-29 13:26:49 -0400

    start Game now has functionality and renders a new piece on each click

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -68,7 +68,7 @@ const Tetris = () => {
             </div>
           )}
           ;
-          <StartButton onClick={startGame} />
+          <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
