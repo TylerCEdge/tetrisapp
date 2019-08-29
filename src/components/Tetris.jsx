ce518a2a2ba461d535ef9a55145debf986161968
TylerCEdge <tylercedge@gmail.com>
2019-08-29 14:22:24 -0400

    added rotate and math to keep blocks from rotating outside game parameter and with other blocks.  Noticed a bug when rotating while up against left/right walls.

--- src/components/Tetris.jsx
+++ src/components/Tetris.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import { createStage } from "../gameHelper";
+
+import { createStage, checkCollision } from "../gameHelper";
 
 // Styled Components
 import { StyledTetrisWrapper, StyledTetris } from "./styles/StyledTetris";
 
 // Custom Hooks
-import { useInterval } from "../hooks/useInterval";
 import { usePlayer } from "../hooks/usePlayer";
 import { useStage } from "../hooks/useStage";
 
@@ -19,22 +19,36 @@ const Tetris = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
-  const [stage, setStage, rowsCleared] = useStage(player);
+  const [stage, setStage] = useStage(player, resetPlayer);
 
-  console.log("re-render");
+  // console.log("re-render");
 
   const movePlayer = dir => {
-    updatePlayerPos({ x: dir, y: 0 });
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 });
+    }
   };
 
   const startGame = () => {
-    // Reset
+    // console.log("test");
+    // Reset everything
     setStage(createStage());
     resetPlayer();
+    setGameOver(false);
   };
 
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false });
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false });
+    } else {
+      // Game Over
+      if (player.pos.y < 1) {
+        console.log("GAME OVER!!!");
+        setGameOver(true);
+        setDropTime(null);
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true });
+    }
   };
 
   const dropPlayer = () => {
@@ -49,6 +63,8 @@ const Tetris = () => {
         movePlayer(1);
       } else if (keyCode === 40) {
         dropPlayer();
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1);
       }
     }
   };
@@ -59,7 +75,7 @@ const Tetris = () => {
         <Stage stage={stage} />
         <aside>
           {gameOver ? (
-            <Display gameOver={gameOver} text="Game Over!" />
+            <Display gameOver={gameOver} text="Game Over" />
           ) : (
             <div>
               <Display text="Score" />
@@ -67,7 +83,6 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          ;
           <StartButton callback={startGame} />
         </aside>
       </StyledTetris>
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,13 +1,42 @@
 import { useState, useCallback } from "react";
-import { randomTetromino } from "../tetroMinos";
-import { STAGE_WIDTH } from "../gameHelper";
+import { TETROMINOS, randomTetromino } from "../tetroMinos";
+import { STAGE_WIDTH, checkCollision } from "../gameHelper";
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
-    tetromino: randomTetromino().shape,
+    tetromino: TETROMINOS[0].shape,
     collided: false
   });
+
+  const rotate = (matrix, dir) => {
+    // Transpose rows to columns
+    const rotatedTetro = matrix.map((_, index) =>
+      matrix.map(col => col[index])
+    );
+    // Reverse each row ot get a rotated matrix
+    if (dir > 0) return rotatedTetro.map(row => row.reverse());
+    return rotatedTetro.reverse();
+  };
+
+  const playerRotate = (stage, dir) => {
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        clonedPlayer.pos.x = pos;
+        return;
+      }
+    }
+
+    setPlayer(clonedPlayer);
+  };
 
   const updatePlayerPos = ({ x, y, collided }) => {
     setPlayer(prev => ({
@@ -25,5 +54,5 @@ export const usePlayer = () => {
     });
   }, []);
 
-  return [player, updatePlayerPos, resetPlayer];
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
 };
