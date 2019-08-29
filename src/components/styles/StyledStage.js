5e80dc24b57d2f4b2af07ddc07997ce15ca5cca1
TylerCEdge <tylercedge@gmail.com>
2019-08-29 12:21:40 -0400

    grid now rendering correctly

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -7,4 +7,9 @@ export const StyledStage = styled.div`
     calc(25vw / ${props => props.width})
   );
   grid-template-columns: repeat(${props => props.width}, 1fr);
-`;
+  grid-gap: 1px;
+  border: 2px solid #333;
+  width: 100%;
+  max-width: 25vw;
+  background: #111;
+`
