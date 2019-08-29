84ab213710f71a5cba7a819fbdb2d4325b87e94e
TylerCEdge <tylercedge@gmail.com>
2019-08-29 13:17:20 -0400

    adding useEffect hook to useStage custom hook

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,7 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createStage } from "../gameHelper";
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+
+  useEffect(() => {
+    const updateStage = prevStage => {
+      // Flush the stage ***change to for loop***
+      const newStage = prevStage.map(row =>
+        row.map(cell => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
+      // Draw tetromino
+      player.tetromino.forEach((row, y) => {
+          row.forEach((value, x) => {
+              if (value !== 0) {
+                  newStage[y + player.pos.y][x + player.pos.x] = [
+                      value
+                  ]
+              }
+          })
+      })
+    };
+
+    setStage(prev => updateStage(prev));
+  }, [
+      player.collided,
+      player.pos.x,
+      player.pos.y,
+      player.tetromino,
+      resetPlayer,
+  ]);
+
   return [stage, setStage];
 };
